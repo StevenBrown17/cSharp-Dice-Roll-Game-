c397c66435e07d9922564a498399d6810ab5fb36
Steven <Steven.Brown17@hotmail.com>
2017-11-13 12:43:17 -0700

    initial commit

---
+++ Assignment2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lostLbl = new System.Windows.Forms.Label();
+            this.wonLbl = new System.Windows.Forms.Label();
+            this.playedLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.rollBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.statsBox = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.winPercentage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lostLbl);
+            this.groupBox1.Controls.Add(this.wonLbl);
+            this.groupBox1.Controls.Add(this.playedLbl);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(35, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(242, 164);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Info";
+            // 
+            // lostLbl
+            // 
+            this.lostLbl.AutoSize = true;
+            this.lostLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lostLbl.Location = new System.Drawing.Point(203, 116);
+            this.lostLbl.Name = "lostLbl";
+            this.lostLbl.Size = new System.Drawing.Size(17, 17);
+            this.lostLbl.TabIndex = 5;
+            this.lostLbl.Text = "0";
+            // 
+            // wonLbl
+            // 
+            this.wonLbl.AutoSize = true;
+            this.wonLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wonLbl.Location = new System.Drawing.Point(203, 78);
+            this.wonLbl.Name = "wonLbl";
+            this.wonLbl.Size = new System.Drawing.Size(17, 17);
+            this.wonLbl.TabIndex = 4;
+            this.wonLbl.Text = "0";
+            // 
+            // playedLbl
+            // 
+            this.playedLbl.AutoSize = true;
+            this.playedLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playedLbl.Location = new System.Drawing.Point(203, 40);
+            this.playedLbl.Name = "playedLbl";
+            this.playedLbl.Size = new System.Drawing.Size(17, 17);
+            this.playedLbl.TabIndex = 3;
+            this.playedLbl.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Times Lost:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Times Won:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Times Played:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Enter your guess (1-6):";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(220, 253);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(35, 20);
+            this.inputBox.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(81, 321);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(96, 97);
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // rollBtn
+            // 
+            this.rollBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollBtn.Location = new System.Drawing.Point(220, 321);
+            this.rollBtn.Name = "rollBtn";
+            this.rollBtn.Size = new System.Drawing.Size(75, 23);
+            this.rollBtn.TabIndex = 4;
+            this.rollBtn.Text = "Roll";
+            this.rollBtn.UseVisualStyleBackColor = true;
+            this.rollBtn.Click += new System.EventHandler(this.rollBtn_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetBtn.Location = new System.Drawing.Point(220, 394);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 5;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // statsBox
+            // 
+            this.statsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statsBox.Location = new System.Drawing.Point(35, 443);
+            this.statsBox.Name = "statsBox";
+            this.statsBox.Size = new System.Drawing.Size(439, 157);
+            this.statsBox.TabIndex = 6;
+            this.statsBox.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(338, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Win Percentage";
+            // 
+            // winPercentage
+            // 
+            this.winPercentage.AutoSize = true;
+            this.winPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winPercentage.Location = new System.Drawing.Point(388, 61);
+            this.winPercentage.Name = "winPercentage";
+            this.winPercentage.Size = new System.Drawing.Size(38, 22);
+            this.winPercentage.TabIndex = 8;
+            this.winPercentage.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 630);
+            this.Controls.Add(this.winPercentage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.statsBox);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.rollBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lostLbl;
+        private System.Windows.Forms.Label wonLbl;
+        private System.Windows.Forms.Label playedLbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button rollBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.RichTextBox statsBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label winPercentage;
+    }
+}
+
---
+++ Assignment2/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1(){
+            InitializeComponent();
+            printStats();
+        }
+
+        #region vars
+
+        /// <summary>
+        /// dieRoll is the random number generated.
+        /// </summary>
+        public int dieRoll;
+        /// <summary>
+        /// played is a counter, counting number of games played
+        /// </summary>
+        public int played;
+        /// <summary>
+        /// won is a counter, counting the number of games won
+        /// </summary>
+        public int won;
+        /// <summary>
+        /// lost is a counter, counting the number of games lost
+        /// </summary>
+        public int lost;
+        /// <summary>
+        /// input holds the value of the user input box. This variable is set in the validateInput(String ...) method
+        /// and should not be hard coded. This variable is only assigned a value if the user input is valid
+        /// </summary>
+        public int input;
+        /// <summary>
+        /// this variable keeps track of the win percentage
+        /// </summary>
+        double winpercent;
+
+        /// <summary>
+        /// headers is an array that will set in the statsBox 
+        /// </summary>
+        String[] headers = { "Face", "Frequency", "Percent", "Number of times guessed" };
+        /// <summary>
+        /// face1 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face1 = { 1, 0, 0 , 0 };
+        /// <summary>
+        /// face2 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face2 = { 2, 0, 0 , 0 };
+        /// <summary>
+        /// face3 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face3 = { 3, 0, 0 , 0 };
+        /// <summary>
+        /// face4 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face4 = { 4, 0, 0 , 0 };
+        /// <summary>
+        /// face5 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face5 = { 5, 0, 0 , 0 };
+        /// <summary>
+        /// face6 array will hold the stats data [0] - Face, [1] - Frequency, [2] - Percent, [3] - Num times guessed
+        /// </summary>
+        public decimal[] face6 = { 6, 0, 0 , 0 };
+
+        #endregion
+
+        #region click actions
+
+        /// <summary>
+        /// rollBtn_Click contains instructions to be performed upon clicking rollBtn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rollBtn_Click(object sender, EventArgs e)
+        {
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (!validateInput(inputBox.Text))
+            {//if validateInput() returns false, will run
+
+                for (int i = 0; i < 7; i++){
+                    Random rnd = new Random();
+                    int dice = rnd.Next(1, 7);
+
+                    pictureBox.Image = Image.FromFile("die" + dice.ToString() + ".gif");
+                    pictureBox.Refresh();
+                    Thread.Sleep(200);
+
+                    //if statement contains methods that determine statistics
+                    if (i == 6){
+                        dieRoll = dice;
+                        determineOutcome(input, dieRoll);
+                        setStats(input, dieRoll);
+                        printStats();
+                    }
+
+                }//end for
+            }//end if
+        }//end rollBtn_click()
+
+        /// <summary>
+        /// resetBtn_Click contains instructions to be performed upon  clicking resetBtn
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resetBtn_Click(object sender, EventArgs e){
+            reset();
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// validateInput(String ...) gets the value from the user input, attempts to parse and check 1-6 conditions. 
+        /// If parse fails or conditions not met, message box informs user that their input was in an invalid format.
+        /// Returns true if parse/conditions fail.
+        /// Returns false if passed.
+        /// bool variable is used to capture method output.
+        /// </summary>
+        /// <param name="inputBoxValue">This is the user input value from the text box</param>
+        /// <returns></returns>
+        private bool validateInput(String inputBoxValue){
+            DialogResult MyResult;
+
+            try{
+                input = Int32.Parse(inputBoxValue);
+
+                if (input != 1 && input != 2 && input != 3 && input != 4 && input != 5 && input != 6){
+                    MyResult = MessageBox.Show("Please enter a number between 1 and 6", "Incorrect Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return true;
+                }
+
+                return false;
+            }
+            catch(Exception e) {
+                MyResult = MessageBox.Show("Please enter a number between 1 and 6", "Incorrect Input", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return true;
+            }//end try catch
+        }//end validateInput()
+
+        /// <summary>
+        /// Increments number of games played, and sets playedLbl
+        /// Compares user input to random number generated, if they are equal, return true and increment won and sets wonLbl.
+        /// if not equal, return false and increment lost and sets lostLbl
+        /// </summary>
+        /// <param name="numberGuessed"></param>
+        /// <param name="numberRolled"></param>
+        /// <returns></returns>
+        private void determineOutcome(int numberGuessed, int numberRolled){
+
+            played++;
+            playedLbl.Text = played.ToString();
+
+            if (numberGuessed == numberRolled){
+                won++;
+                wonLbl.Text = won.ToString();
+            }
+            else{
+                lost++;
+                lostLbl.Text = lost.ToString();
+            }
+
+            winpercent = ((double)won / (double)played) *100;
+            winpercent = Math.Round(winpercent, 2);
+            winPercentage.Text = winpercent +"%";
+
+        }//end determineOutcome()
+
+        /// <summary>
+        /// resets all vars and labels to default vales
+        /// </summary>
+        public void reset(){
+            played = 0;
+            won = 0;
+            lost = 0;
+            winpercent = 0;
+            playedLbl.Text = "0";
+            wonLbl.Text = "0";
+            lostLbl.Text = "0";
+            winPercentage.Text = "0%";
+            inputBox.Clear();
+            face1[1] = 0; face1[2] = 0; face1[3] = 0;
+            face2[1] = 0; face2[2] = 0; face2[3] = 0;
+            face3[1] = 0; face3[2] = 0; face3[3] = 0;
+            face4[1] = 0; face4[2] = 0; face4[3] = 0;
+            face5[1] = 0; face5[2] = 0; face5[3] = 0;
+            face6[1] = 0; face6[2] = 0; face6[3] = 0;
+
+            printStats();
+        }
+        
+
+        /// <summary>
+        /// printStats() concats headers and face arrays together, then sets the statsBox textarea.
+        /// </summary>
+        public void printStats(){
+
+            //sets tab spacing
+            statsBox.SelectionTabs = new int[] { 90, 180, 270, 360 };
+            
+            String stats="";//initial string
+
+            #region for loops
+
+            for (int i = 0; i < headers.Length; i++){
+                stats += headers[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face1.Length; i++){
+                if(i==2) stats += face1[i] + "%\t";
+                else stats += face1[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face2.Length; i++){
+                if(i==2) stats += face2[i] + "%\t";
+                else stats += face2[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face3.Length; i++){
+                if (i == 2) stats += face3[i] + "%\t";
+                else stats += face3[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face4.Length; i++){
+                if (i == 2) stats += face4[i] + "%\t";
+                else stats += face4[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face5.Length; i++){
+                if (i == 2) stats += face5[i] + "%\t";
+                else stats += face5[i] + "\t";
+            }
+
+            stats += "\n";
+            for (int i = 0; i < face6.Length; i++){
+                if (i == 2) stats += face6[i] + "%\t";
+                else stats += face6[i] + "\t";
+            }
+
+            #endregion
+
+            statsBox.Text = stats; //sets textbox
+
+        }
+
+        /// <summary>
+        /// setStats(int ..., int ...) calculates the information needed to display in the statsBox textbox
+        /// </summary>
+        /// <param name="numGuessed"></param>
+        /// <param name="numRolled"></param>
+        public void setStats(int numGuessed, int numRolled){
+            //double[] face1 = { 1, 0, 0.00, 0 };
+
+            switch (numGuessed){
+                            case 1:
+                                face1[3]++;
+                                break;
+                            case 2:
+                                face2[3]++;
+                                break;
+                            case 3:
+                                face3[3]++;
+                                break;
+                            case 4:
+                                face4[3]++;
+                                break;
+                            case 5:
+                                face5[3]++;
+                                break;
+                            case 6:
+                                face6[3]++;
+                                break;
+                            default:
+                                break;
+
+                        }//end switch
+
+            switch (numRolled){
+                case 1:
+                    face1[1]++;
+                    break;
+                case 2:
+                    face2[1]++;
+                    break;
+                case 3:
+                    face3[1]++;
+                    break;
+                case 4:
+                    face4[1]++;
+                    break;
+                case 5:
+                    face5[1]++;
+                    break;
+                case 6:
+                    face6[1]++;
+                    break;
+                default:
+                    break;
+
+            }//end switch
+
+            for (int i = 0; i < face1.Length; i++){
+                face1[2] = face1[1] / played * 100;
+                face2[2] = face2[1] / played * 100;
+                face3[2] = face3[1] / played * 100;
+                face4[2] = face4[1] / played * 100;
+                face5[2] = face5[1] / played * 100;
+                face6[2] = face6[1] / played * 100;
+            }
+            face1[2]=Math.Round(face1[2], 2);
+            face2[2]=Math.Round(face2[2], 2);
+            face3[2]=Math.Round(face3[2], 2);
+            face4[2]=Math.Round(face4[2], 2);
+            face5[2]=Math.Round(face5[2], 2);
+            face6[2]=Math.Round(face6[2], 2);
+
+
+        }//end setStats
+
+        #endregion
+
+
+    }//end class
+}//end name space
